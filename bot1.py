8d74bcd069e6e466667634233fb1c3b029f85e38
Ilhomjon777 <102343562+Ilhomjon777@users.noreply.github.com>
2025-02-12 21:48:16 +0500

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,19 +1,19 @@
 from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, MessageHandler, filters, CallbackContext
 
 BOT_1_TOKEN = "7687555090:AAFaGqPhqX7j3sLvYUzRqEhjYhKu1zmhQ3o"
-CHAT_ID_BOT_2 = "6349583688"
+CHAT_ID_BOT_2 = 6349583688  # Chat ID butun son bo‘lishi kerak
 
-def forward_to_bot_2(update: Update, context: CallbackContext) -> None:
+async def forward_to_bot_2(update: Update, context: CallbackContext) -> None:
     message = update.message
-    message.forward(chat_id=CHAT_ID_BOT_2)
+    await message.forward(chat_id=CHAT_ID_BOT_2)
 
 def main():
-    updater = Updater(BOT_1_TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_to_bot_2))
-    updater.start_polling()
-    updater.idle()
+    app = Application.builder().token(BOT_1_TOKEN).build()
+    
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_bot_2))
+    
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
